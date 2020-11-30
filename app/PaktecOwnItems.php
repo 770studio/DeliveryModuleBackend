4bc8443a9b1622b6b5684fdd52c62bce0ecd61c6
Илья <vayner.ilya@ya.ru>
2020-11-30 14:29:50 +0300

    Paktec

--- app/PaktecOwnItems.php
+++ app/PaktecOwnItems.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class PaktecOwnItems extends Model
 {
    // protected $fillable = ['title', 'url', 'track' ];
-    protected $guarded = [];
+   // protected $guarded = [];
     protected $table = 'paktec_own_items';
-    //protected $fillable = ['title', 'url', 'track', 'price', 'listing_id', 'best_seller'];
+    protected $fillable = ['title', 'url', 'track', 'price', 'listing_id', 'best_seller'];
 
     public function competitors()
     {
